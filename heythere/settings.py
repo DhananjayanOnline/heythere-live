d7f01f45e17a37ee0133587514f8f8c00dcefa8d
Dhananjayan <dhananjayan.online@outlook.com>
2022-08-30 22:07:18 +0530

    heroku deployment phase 1

--- heythere/settings.py
+++ heythere/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h+m+k)tdy=m%l=nd#f1y60@0zavaoop-k#yaa*@yv!(urwhz-*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['heythere.azurewebsites.net', '127.0.0.1']
+ALLOWED_HOSTS = ['heythere.azurewebsites.net', '127.0.0.1', 'heythere-india.herokuapp.com']
 
 
 # Application definition
