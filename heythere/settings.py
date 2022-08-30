61385dbd1f397cf852cc963aaa383b802d45c844
Dhananjayan <dhananjayan.online@outlook.com>
2022-08-30 22:16:43 +0530

    static file root updated - heroku deployement phase 2

--- heythere/settings.py
+++ heythere/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/images/'
 
